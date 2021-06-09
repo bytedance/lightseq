42fbf877c173422f8812a3bedb96331fbf373926
Xingyao Wang <wangxingyao@bytedance.com>
2021-06-09 17:13:57 +0800

    updated tokenizer for ls_gpt to deal with different padding

--- example/python/ls_gpt.py
+++ example/python/ls_gpt.py
@@ -2,6 +2,7 @@ import time
 import argparse
 
 import torch
+import numpy as np
 import lightseq
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
 
@@ -14,41 +15,55 @@ def ls_gpt2(model, inputs):
     end_time = time.perf_counter()
     return generated_ids, end_time-start_time
 
+
 def hf_gpt2(model, inputs):
     torch.cuda.synchronize()
     start_time = time.perf_counter()
-    generated_ids = model.generate(inputs.to('cuda:0'), max_length=50)
+    generated_ids = model.generate(
+        inputs["input_ids"].to('cuda:0'),
+        max_length=50,
+    )
     torch.cuda.synchronize()
     end_time = time.perf_counter()
     return generated_ids, end_time-start_time
 
-def ls_generate(model, tokenizer, inputs_id):
+
+def ls_generate(model, tokenizer, inputs):
     print("=========lightseq=========")
     print("lightseq generating...")
-    ls_res_ids, ls_time = ls_gpt2(model, inputs_id)
-    ls_res_ids = [ids[0] for ids in ls_res_ids[0]]
+    ls_res_ids, ls_time = ls_gpt2(model, inputs["input_ids"])
     ls_res = tokenizer.batch_decode(ls_res_ids, skip_special_tokens=True)
     print(f"lightseq time: {ls_time}s")
     print("lightseq results:")
     for sent in ls_res:
         print(sent)
+        print("------")
+    print(ls_res_ids)
 
-def hf_generate(model, tokenizer, inputs_id):
+
+def hf_generate(model, tokenizer, inputs):
     print("=========huggingface=========")
     print("huggingface generating...")
-    hf_res_ids, hf_time = hf_gpt2(model, inputs_id)
+    hf_res_ids, hf_time = hf_gpt2(model, inputs)
     hf_res = tokenizer.batch_decode(hf_res_ids, skip_special_tokens=True)
     print(f"huggingface time: {hf_time}s")
     print("huggingface results:")
     for sent in hf_res:
         print(sent)
+        print("------")
+    print(hf_res_ids)
 
-def warmup(tokenizer, ls_model, hf_model, sentences):
-    inputs = tokenizer(sentences, return_tensors="pt", padding=True)
-    inputs_id = inputs["input_ids"]
 
-    ls_generate(ls_model, tokenizer, inputs_id)
-    hf_generate(hf_model, tokenizer, inputs_id)
+def warmup(ls_tokenizer, hf_tokenizer, ls_model, hf_model, sentences):
+    ls_inputs = ls_tokenizer(sentences, return_tensors="pt", padding=True)
+    # ls_input_ids = ls_inputs["input_ids"]
+
+    hf_inputs = hf_tokenizer(sentences, return_tensors="pt", padding=True)
+    # hf_input_ids = hf_inputs["input_ids"]
+
+    ls_generate(ls_model, ls_tokenizer, ls_inputs)
+    hf_generate(hf_model, hf_tokenizer, hf_inputs)
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -56,23 +71,37 @@ def main():
     args = parser.parse_args()
 
     print("initializing gpt tokenizer...")
-    tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+    ls_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    # lightseq use len(tokenizer) as pad_token in default
+    ls_tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    print(f"lightseq tokenizer pad token id: {ls_tokenizer.pad_token_id}")
+
+    hf_tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+    # use EOS as PAD for huggingface to avoid warning according to https://huggingface.co/blog/how-to-generate while avoid reshaping the model embedding
+    hf_tokenizer.pad_token = hf_tokenizer.eos_token
+    print(f"huggingface tokenizer pad token id: {hf_tokenizer.pad_token_id}")
 
     print("creating lightseq model...")
-    ls_model = lightseq.Gpt("lightseq_gpt2.pb", 128)
+    ls_model = lightseq.Gpt(
+        "lightseq_gpt2.pb",
+        max_batch_size=16,
+        max_step=50
+    )
+
     print("creating huggingface model...")
     hf_model = GPT2LMHeadModel.from_pretrained("gpt2")
     hf_model.to('cuda:0')
 
     sentences = [
-        "I love that girl, but <mask> does not <mask> me.",
-        "She is so <mask> that I can not help glance at <mask>.",
-        "Nothing's gonna <mask> my love for you.",
-        "Drop everything now. Meet me in the pouring <mask>. Kiss me on the sidewalk."
+        "I love that girl, but",
+        # "She is so beautiful that I can not help",
+        # "Nothing's gonna stop",
+        # "Drop everything now. Meet me in the pouring"
     ]
 
     print("====================START warmup====================")
-    warmup(tokenizer, ls_model, hf_model, sentences)
+    warmup(ls_tokenizer, hf_tokenizer, ls_model, hf_model, sentences)
     print("====================END warmup====================")
 
     while True:
@@ -80,14 +109,20 @@ def main():
             sentences = [input("input the masked sentence:\n")]
 
         print("tokenizing the sentences...")
-        inputs = tokenizer(sentences, return_tensors="pt", padding=True)
-        inputs_id = inputs["input_ids"]
 
-        ls_generate(ls_model, tokenizer, inputs_id)
-        hf_generate(hf_model, tokenizer, inputs_id)
+        ls_inputs = ls_tokenizer(sentences, return_tensors="pt", padding=True)
+
+        hf_inputs = hf_tokenizer(sentences, return_tensors="pt", padding=True)
+
+        print(hf_inputs)
+        print(ls_inputs)
+
+        ls_generate(ls_model, ls_tokenizer, ls_inputs)
+        hf_generate(hf_model, hf_tokenizer, hf_inputs)
 
         if not args.user_input:
             break
+
 
 if __name__ == "__main__":
     main()
