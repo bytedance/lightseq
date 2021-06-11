9b914c8bd2eed8f47ece2f0aa5cf55ecdce8f99c
xiongying.taka <xiongying.taka@bytedance.com>
2021-06-11 15:30:39 +0800

    format with black

--- example/python/ls_gpt_time_test.py
+++ example/python/ls_gpt_time_test.py
@@ -10,19 +10,19 @@ def ls_gpt(model, inputs):
     generated_ids = model.sample(inputs)
     torch.cuda.synchronize()
     end_time = time.perf_counter()
-    return generated_ids, end_time-start_time
+    return generated_ids, end_time - start_time
+
 
 def hf_gpt(model, inputs, tokenizer):
     torch.cuda.synchronize()
     start_time = time.perf_counter()
     generated_ids = model.generate(
-        inputs,
-        max_length=50,
-        pad_token_id=tokenizer.eos_token_id
+        inputs, max_length=50, pad_token_id=tokenizer.eos_token_id
     )
     torch.cuda.synchronize()
     end_time = time.perf_counter()
-    return generated_ids, end_time-start_time
+    return generated_ids, end_time - start_time
+
 
 def main():
     print("initializing gpt tokenizer...")
@@ -33,13 +33,13 @@ def main():
     ls_model = lightseq.Gpt(
         "lightseq_gpt2.pb",
         max_batch_size=128,
-        max_step=50, # max length
+        max_step=50,  # max length
     )
     print("creating huggingface model...")
     hf_model = GPT2LMHeadModel.from_pretrained("gpt2")
 
     # GPU warm up
-    sentences = [' '.join(['I'] * 10)] * 8
+    sentences = [" ".join(["I"] * 10)] * 8
     inputs = tokenizer(sentences, return_tensors="pt", padding=True)
     inputs_id = inputs["input_ids"]
     _, _ = ls_gpt(ls_model, inputs_id)
@@ -51,7 +51,7 @@ def main():
         total_ls = 0.0
         total_hf = 0.0
         for seq_len in seq_len_list:
-            sentences = [' '.join(['I'] * seq_len)] * bsz
+            sentences = [" ".join(["I"] * seq_len)] * bsz
             inputs = tokenizer(sentences, return_tensors="pt", padding=True)
             inputs_id = inputs["input_ids"]
             _, ls_time = ls_gpt(ls_model, inputs_id)
@@ -59,6 +59,7 @@ def main():
             total_ls += ls_time
             total_hf += hf_time
         print(f"{bsz}: {total_hf/total_ls-1}")
+
 
 if __name__ == "__main__":
     main()
