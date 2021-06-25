1609069bc75669644694f4448c2296ec8da09db8
Ying Xiong <xiongying.taka@bytedance.com>
2021-06-25 13:23:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -92,7 +92,7 @@ with open("README.md", "r") as fh:
 
 setup_kwargs = dict(
     name="lightseq",
-    version="2.0.0",
+    version="2.0.2",
     author="Xiaohui Wang, Ying Xiong, Xian Qian, Yang Wei",
     author_email="wangxiaohui.neo@bytedance.com, xiongying.taka@bytedance.com"
     ", qian.xian@bytedance.com, weiyang.god@bytedance.com",
