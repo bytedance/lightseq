1213f22a852a446c5c1aadc02e0661b18e2e3553
Xingyao Wang <wangxingyao@bytedance.com>
2021-06-28 15:13:04 +0800

    update hdf5 dataset utility

--- lightseq/inference/tools/util.cc.cu
+++ lightseq/inference/tools/util.cc.cu
@@ -175,5 +175,73 @@ bool endswith(std::string const& full, std::string const& end) {
   return false;
 }
 
+int get_hdf5_dataset_size(hid_t dataset) {
+  hid_t dataspace = H5Dget_space(dataset); /* dataspace handle */
+  int n_dims = H5Sget_simple_extent_ndims(dataspace);
+  // return 1 for scalar
+  if (n_dims < 1) {
+    return 1;
+  }
+  // get dimensions for N-Dimension vector
+  hsize_t dims[n_dims];
+  int status = H5Sget_simple_extent_dims(dataspace, dims, NULL);
+  if (status != n_dims || status < 0) {
+    // return negative number on error
+    return -1;
+  }
+  // accumulate size from every dimension
+  int vec_size = 1;
+  for (int i = 0; i < n_dims; ++i) {
+    vec_size *= dims[i];
+  }
+  return vec_size;
+}
+
+int get_hdf5_dataset_size(hid_t hdf5_file, std::string& dataset_name) {
+  // parse dataset size
+  hid_t ds = H5Dopen2(hdf5_file, dataset_name, H5P_DEFAULT);
+  if (ds < 0) {
+    throw std::runtime_error("Failed to open HDF5 dataset: " + dataset_name);
+  }
+  int ds_size = get_hdf5_dataset_size(ds);
+  if (ds_size < 0) {
+    throw std::runtime_error("HDF5 parsing error: " + dataset_name);
+  }
+  H5Dclose(ds);
+  return ds_size
+}
+
+int read_hdf5_dataset_data(
+    hid_t hdf5_file, std::string& dataset_name, hid_t output_type,
+    void* output_buf,
+    std::function<bool(int)> size_predicate = [](int x) -> bool {
+      return (x < 0);
+    },
+    std::string extra_msg = "") {
+  hid_t ds = H5Dopen2(hdf5_file, dataset_name, H5P_DEFAULT);
+  int ds_size = get_hdf5_dataset_size(ds);
+
+  // sanity (custom) check for size with extra message.
+  if (size_predicate(ds_size)) {
+    throw std::runtime_error("Invalid shape " + ds_size + ". " + extra_msg);
+  }
+
+  herr_t status =
+      H5Dread(ds, output_type, H5S_ALL, H5S_ALL, H5P_DEFAULT, output_buf);
+
+  if (status < 0) {
+    throw std::runtime_error("Failed to read HDF5 dataset: " + dataset_name);
+  }
+  H5Dclose(ds);
+  return ds_size;
+}
+
+void read_hdf5_dataset_scalar(hid_t hdf5_file, std::string& dataset_name,
+                              hid_t output_type, void* output_buf) {
+  read_hdf5_dataset_data(
+      hdf5_file, dataset_name, output_type, output_buf,
+      [](int size) { return size != 1 }, "Expect scalar with shape of 1.");
+}
+
 }  // namespace cuda
 }  // namespace lightseq
--- lightseq/inference/tools/util.h
+++ lightseq/inference/tools/util.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 #include <cublas_v2.h>
 #include <cuda.h>
@@ -13,6 +14,7 @@
 #include <thrust/device_vector.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/random.h>
+#include "hdf5.h"
 
 /**
 @file
@@ -147,6 +149,43 @@ void read_batch_tokenids_from_file(std::string, int& batch_size,
 Utility function for initializing
 */
 bool endswith(std::string const& full, std::string const& end);
+
+/*
+Helper function of HDF5.
+
+Return the 1D size of given hdf5 dataset if dataset is already open.
+*/
+int get_hdf5_dataset_size(hid_t dataset);
+
+/*
+Helper function of HDF5.
+
+Return the 1D size of given hdf5 dataset in the given file.
+*/
+int get_hdf5_dataset_size(hid_t hdf5_file, std::string& dataset_name);
+
+/*
+Helper function of HDF5.
+
+Read the data of specified type `output_type` into `output_buf`.
+
+returns: the size of output data
+*/
+int read_hdf5_dataset_data(
+    hid_t hdf5_file, std::string& dataset_name, hid_t output_type,
+    void* output_buf,
+    std::function<bool(int)> size_predicate = [](int x) -> bool {
+      return (x < 0);
+    },
+    std::string extra_msg = "");
+
+/*
+Helper function of HDF5.
+
+Read a scalar of specified type `output_type` into `output_buf`.
+*/
+void read_hdf5_dataset_scalar(hid_t hdf5_file, std::string& dataset_name,
+                              hid_t output_type, void* output_buf);
 
 }  // namespace cuda
 }  // namespace lightseq
