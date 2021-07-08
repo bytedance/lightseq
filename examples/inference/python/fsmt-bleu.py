72974d647e09cd6b8cc8d616f84d4d574769f773
Xingyao Wang <wangxingyao@bytedance.com>
2021-07-08 15:40:35 +0800

    output translation file for later comparison; add dataset sentence length calculation;

--- examples/inference/python/fsmt-bleu.py
+++ examples/inference/python/fsmt-bleu.py
@@ -15,10 +15,6 @@ mname = "facebook/wmt19-en-de"
 
 tokenizer = FSMTTokenizer.from_pretrained(mname)
 
-# load models 
-hf_model = FSMTForConditionalGeneration.from_pretrained(mname).cuda()
-ls_model = lsi.Transformer("lightseq_fsmt_wmt19ende.hdf5", 128)
-
 # load dataset: en -> de translation
 eval_dataset = raw_datasets["validation"]
 column_names = eval_dataset.column_names
@@ -28,11 +24,17 @@ def preprocess_function(examples):
     targets = [ex[tgt_lang] for ex in examples["translation"]]
     inputs = [inp for inp in inputs]
     model_inputs = tokenizer(inputs, padding=True, pad_to_multiple_of=8)
-
     # Setup the tokenizer for targets
     with tokenizer.as_target_tokenizer():
         labels = tokenizer(targets, padding=True, pad_to_multiple_of=8)
+    
+    # calculate length in dataset, determine max_step to be approx >= 200
+    # print(model_inputs['input_ids'])
+    # print(f"en max len: {max(list(map(len, model_inputs['input_ids'])))}")
 
+    # print(labels['input_ids'])
+    # print(f"de max len: {max(list(map(len, labels['input_ids'])))}")
+    
     model_inputs["labels"] = labels["input_ids"]
     return model_inputs
 
@@ -49,38 +51,65 @@ data_collator = DataCollatorForSeq2Seq(
     label_pad_token_id=tokenizer.pad_token_id,
     pad_to_multiple_of=8,
 )
-eval_dataloader = DataLoader(eval_dataset, collate_fn=data_collator, batch_size=128)
+eval_dataloader = DataLoader(eval_dataset, collate_fn=data_collator, batch_size=32)
 
 # initialize metric
 ls_metric = load_metric("sacrebleu")
 hf_metric = load_metric("sacrebleu")
 
-for step, batch in tqdm(enumerate(eval_dataloader), total=len(eval_dataloader)):
-    with torch.no_grad():
-        input_ids = batch["input_ids"]
-        labels = batch["labels"]
-        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-        decoded_labels = [[label.strip()] for label in decoded_labels]
+def run_ls():
+    ls_model = lsi.Transformer("lightseq_fsmt_wmt19ende.hdf5", 32) # 2nd argument is max_batch_size
+    for step, batch in tqdm(enumerate(eval_dataloader), total=len(eval_dataloader)):
+        with torch.no_grad():
+            input_ids = batch["input_ids"]
+            labels = batch["labels"]
+            decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+            decoded_labels = [[label.strip()] for label in decoded_labels]
 
-        # huggingface generation
-        hf_generated_tokens = hf_model.generate(
-            input_ids.cuda(),
-            # attention_mask=batch["attention_mask"],
-            max_length=128,
-            num_beams=64,
-        )
-        hf_decoded_preds = tokenizer.batch_decode(hf_generated_tokens, skip_special_tokens=True)
-        hf_decoded_preds = [pred.strip() for pred in hf_decoded_preds]
-        hf_metric.add_batch(predictions=hf_decoded_preds, references=decoded_labels)
+            # lightseq generation
+            ls_generated_tokens = ls_model.infer(
+                input_ids,
+            )
+            ls_generated_tokens = [ids[0] for ids in ls_generated_tokens[0]]
+            ls_decoded_preds = tokenizer.batch_decode(ls_generated_tokens, skip_special_tokens=True)
+            ls_decoded_preds = [pred.strip() for pred in ls_decoded_preds]
+            ls_out.write("\n".join(ls_decoded_preds))
+            ls_metric.add_batch(predictions=ls_decoded_preds, references=decoded_labels)
 
-        # lightseq generation
-        ls_generated_tokens = ls_model.infer(
-            input_ids,
-        )
-        ls_generated_tokens = [ids[0] for ids in ls_generated_tokens[0]]
-        ls_decoded_preds = tokenizer.batch_decode(ls_generated_tokens, skip_special_tokens=True)
-        ls_decoded_preds = [pred.strip() for pred in ls_decoded_preds]
-        ls_metric.add_batch(predictions=ls_decoded_preds, references=decoded_labels)
+def run_hf():
+    # load models 
+    hf_model = FSMTForConditionalGeneration.from_pretrained(mname).cuda()
+
+    for step, batch in tqdm(enumerate(eval_dataloader), total=len(eval_dataloader)):
+        with torch.no_grad():
+            input_ids = batch["input_ids"]
+            labels = batch["labels"]
+            decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+            # save reference output here
+            ref_out.write("\n".join([label.strip() for label in decoded_labels]))
+            decoded_labels = [[label.strip()] for label in decoded_labels]
+
+            # huggingface generation
+            hf_generated_tokens = hf_model.generate(
+                input_ids.cuda(),
+                # attention_mask=batch["attention_mask"],
+                max_length=256,
+                num_beams=8,
+            )
+            hf_decoded_preds = tokenizer.batch_decode(hf_generated_tokens, skip_special_tokens=True)
+            hf_decoded_preds = [pred.strip() for pred in hf_decoded_preds]
+            hf_out.write("\n".join(hf_decoded_preds))
+            hf_metric.add_batch(predictions=hf_decoded_preds, references=decoded_labels)
+
+
+hf_out = open("translation-hf.txt", "w")
+ls_out = open("translation-ls.txt", "w")
+ref_out = open("translation-reference.txt", "w")
+
+print(f"running huggingface translation...")
+run_hf()
+print(f"running lightseq translation...")
+run_ls()
 
 ls_eval_metric = ls_metric.compute()
 hf_eval_metric = hf_metric.compute()
@@ -88,3 +117,7 @@ print({
     "lightseq bleu": ls_eval_metric["score"],
     "huggingface bleu": hf_eval_metric["score"]
 })
+
+hf_out.close()
+ls_out.close()
+ref_out.close()
