55e58fc53c90a23c72f70023f74d556eb1d84084
godweiyang <godweiyang@gmail.com>
2021-07-16 18:23:40 +0800

    Simplify the path of layer calls

---
+++ lightseq/training/__init__.py
@@ -0,0 +1,5 @@
+from lightseq.training.ops.pytorch.transformer_embedding_layer import *
+from lightseq.training.ops.pytorch.transformer_encoder_layer import *
+from lightseq.training.ops.pytorch.transformer_decoder_layer import *
+from lightseq.training.ops.pytorch.cross_entropy_layer import *
+from lightseq.training.ops.pytorch.adam import *
