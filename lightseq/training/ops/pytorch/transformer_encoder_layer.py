2505fcc5eea03080835f992d29240b2111acff9e
godweiyang <godweiyang@gmail.com>
2021-07-18 20:33:44 +0800

    add default architecture for encoder and decoder

--- lightseq/training/ops/pytorch/transformer_encoder_layer.py
+++ lightseq/training/ops/pytorch/transformer_encoder_layer.py
@@ -12,6 +12,46 @@ from lightseq.training.ops.pytorch.util import copy_para
 transformer_cuda_module = None
 _all_layer_grads = dict()
 
+def base_architecture(args):
+    args.setdefault("hidden_size", 512)
+    args.setdefault("intermediate_size", 2048)
+    args.setdefault("nhead", 8)
+    args.setdefault("attn_prob_dropout_ratio", 0.0)
+    args.setdefault("activation_dropout_ratio", 0.0)
+    args.setdefault("hidden_dropout_ratio", 0.1)
+    args.setdefault("pre_layer_norm", True)
+
+def transformer_big(args):
+    args.setdefault("hidden_size", 1024)
+    args.setdefault("intermediate_size", 4096)
+    args.setdefault("nhead", 16)
+    args.setdefault("attn_prob_dropout_ratio", 0.1)
+    args.setdefault("activation_dropout_ratio", 0.1)
+    base_architecture(args)
+
+def transformer_base(args):
+    base_architecture(args)
+
+def bert_base(args):
+    args.setdefault("hidden_size", 768)
+    args.setdefault("intermediate_size", 3072)
+    args.setdefault("nhead", 12)
+    args.setdefault("attn_prob_dropout_ratio", 0.1)
+    args.setdefault("activation_dropout_ratio", 0.1)
+    args.setdefault("pre_layer_norm", False)
+    base_architecture(args)
+
+def bert_big(args):
+    args.setdefault("pre_layer_norm", False)
+    transformer_big(args)
+
+MODEL_ARCH = {
+    "transformer-base": transformer_base,
+    "transformer-big": transformer_big,
+    "bert-base": bert_base,
+    "bert-big": bert_big,
+}
+
 
 class LSTransformerEncoderFunc(Function):
     @staticmethod
@@ -187,6 +227,12 @@ class LSTransformerEncoderLayer(nn.Module):
             pre_layer_norm: bool  # pre layer norm or post
             fp16: bool  # fp16 presion
             local_rank: int  # rank in local node
+
+        if "model" in kwargs:
+            if kwargs["model"] not in MODEL_ARCH:
+                raise ValueError("{} architecture is not supported.")
+            MODEL_ARCH[kwargs["model"]](kwargs)
+            del kwargs["model"]
 
         return Config(**kwargs)
 
