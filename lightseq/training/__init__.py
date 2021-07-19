bd3db82f1064f5ea1c481d1614970dcb3b9453d8
godweiyang <godweiyang@gmail.com>
2021-07-19 16:00:52 +0800

    add python wrapper for ls transformer

--- lightseq/training/__init__.py
+++ lightseq/training/__init__.py
@@ -7,5 +7,6 @@ from lightseq.training.ops.pytorch.transformer_encoder_layer import (
 from lightseq.training.ops.pytorch.transformer_decoder_layer import (
     LSTransformerDecoderLayer,
 )
+from lightseq.training.ops.pytorch.transformer import LSTransformer
 from lightseq.training.ops.pytorch.cross_entropy_layer import LSCrossEntropyLayer
 from lightseq.training.ops.pytorch.adam import LSAdam
