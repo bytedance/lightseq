dd1692bf7b780986aada5c531893bb904d07dfa5
weiyang.god <weiyang.god@bytedance.com>
2021-10-08 15:32:03 +0800

    fix format

--- lightseq/training/csrc/ops/includes/feed_forward_v4.h
+++ lightseq/training/csrc/ops/includes/feed_forward_v4.h
@@ -33,8 +33,7 @@ class FeedForwardV4 {
   ~FeedForwardV4() {}
 
   void Forward(const int8_t *A, const T *B, T *C, int8_t *B_buffer,
-               int32_t *C_buffer, cublasHandle_t handle,
-               cudaStream_t stream) {
+               int32_t *C_buffer, cublasHandle_t handle, cudaStream_t stream) {
     int m = _config.m, n = _config.n, k = _config.k;
     int align = 16;
     n = (n + align - 1) / align * align;
@@ -42,9 +41,8 @@ class FeedForwardV4 {
     float scale_A = 127, scale_B = 127, clip_max_A = 0.3, clip_max_B = 16.0;
     launch_quantize_tensor(B, B_buffer, n * k, scale_B, clip_max_B, stream);
 
-    cublas_gemm_ex(handle, CUBLAS_OP_T, CUBLAS_OP_N, _config.m,
-                   n, _config.k, &alpha, &beta, A, B_buffer,
-                   C_buffer, cublasGemmAlgo_t(99));
+    cublas_gemm_ex(handle, CUBLAS_OP_T, CUBLAS_OP_N, _config.m, n, _config.k,
+                   &alpha, &beta, A, B_buffer, C_buffer, cublasGemmAlgo_t(99));
 
     launch_dequantize_tensor(C_buffer, C, m * _config.n, scale_A * scale_B,
                              clip_max_A * clip_max_B, stream);
