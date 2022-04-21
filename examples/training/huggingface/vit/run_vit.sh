bb517733d92cf69cc0f612df0eacf8f293299829
zhangzhexi <zhangzhexi@bytedance.com>
2022-04-21 14:53:47 +0800

    update format of run_vit.sh

--- examples/training/huggingface/vit/run_vit.sh
+++ examples/training/huggingface/vit/run_vit.sh
@@ -1,8 +1,24 @@
-#! /bin/bash
+# Copyright 2021 The LightSeq Team
+# Copyright 2020 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-python3 run_vit.py \
+THIS_DIR=$(dirname $(readlink -f $0))
+
+python3 $THIS_DIR/run_vit.py \
     --dataset_name beans \
-    --output_dir ./beans_outputs/ \
+    --output_dir /tmp/beans_outputs \
+    --overwrite_output_dir \
     --remove_unused_columns False \
     --do_train \
     --do_eval \
