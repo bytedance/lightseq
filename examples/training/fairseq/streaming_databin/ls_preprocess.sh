f7f9df0e86feec3acd46c2af7f78c992cde58b34
duanrenchong <duanrenchong@bytedance.com>
2022-07-25 11:12:45 +0800

    init streaming dataset

---
+++ examples/training/fairseq/streaming_databin/ls_preprocess.sh
@@ -0,0 +1,8 @@
+# --text-dir: Directory path containing the train valid test
+# --n-line-per-file: Number of lines per file, default 100w lines
+
+text=/opt/tiger/begin/wmt14_en_fr
+# Other parameters are the same as fairseq-preprocess
+bash preprocess_splitk.sh \
+    --source-lang en --target-lang fr \
+    --text-dir $text --destdir databin --n-line-per-file 1000000
