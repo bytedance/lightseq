252e1de4dee55988888ff0964d8b7672d9752254
duanrenchong <duanrenchong@bytedance.com>
2022-07-25 19:51:24 +0800

    fixs

--- examples/training/fairseq/streaming_databin/ls_preprocess.sh
+++ examples/training/fairseq/streaming_databin/ls_preprocess.sh
@@ -1,8 +1,11 @@
+#!/usr/bin/env bash
+set -ex
+THIS_DIR=$(dirname $(readlink -f $0))
+cd $THIS_DIR/../../..
+
 # --text-dir: Directory path containing the train valid test
 # --n-line-per-file: Number of lines per file, default 100w lines
-
-text=/opt/tiger/begin/wmt14_en_fr
 # Other parameters are the same as fairseq-preprocess
 bash preprocess_streaming.sh \
     --source-lang en --target-lang fr \
-    --text-dir $text --destdir databin --n-line-per-file 1000000
+    --text-dir /path/to/text_dir --destdir databin --n-line-per-file 1000000
