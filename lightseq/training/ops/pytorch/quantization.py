74aff2a74f29dd905194db24090e1f92c33b2879
huxingwu.lidao <huxingwu.lidao@bytedance.com>
2022-07-27 10:44:31 +0800

    add backward qat

--- lightseq/training/ops/pytorch/quantization.py
+++ lightseq/training/ops/pytorch/quantization.py
@@ -1,8 +1,11 @@
 import torch.nn.functional as F
+import torch
 from torch.nn import Linear
+from torch.autograd import Function
 from lightseq.training.pytorch_quantization.tensor_quant import (
     QuantDescriptor,
     QUANT_DESC_8BIT_PER_TENSOR,
+    _tensor_quant,
 )
 from lightseq.training.pytorch_quantization.nn.modules.tensor_quantizer import (
     TensorQuantizer,
@@ -11,6 +14,60 @@ from lightseq.training.pytorch_quantization.nn.modules.tensor_quantizer import (
     qat_mode,
     ptq_mode,
 )
+
+class LSLinear(Function):
+    @staticmethod
+    def forward(ctx, inp, weight):
+
+        output = F.linear(inp, weight)
+
+        ctx.save_for_backward(inp, weight)
+        return output
+
+    @staticmethod
+    def quant_transform(tensor):
+        tensor_max = tensor.abs().flatten().topk(100)[0][-1]
+        out_dtype = torch.float16
+        tensor, scale = _tensor_quant(tensor, tensor_max, 8, False, True)
+        tensor = (tensor * scale).to(out_dtype)
+        return tensor
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        (inp, weight) = ctx.saved_tensors
+        out_size, in_size = weight.size()
+        bsz, seq_len, _ = inp.size()
+        # out_dtype = grad_output.dtype
+        # reduce_dim = tuple(range(grad_output.dim() - 1))
+        # grad_output_max = grad_output.abs().max()
+        # grad_output_max = grad_output.abs().amax(reduce_dim)
+        # grad_output_max = grad_output.abs().float().quantile(0.9999)
+        # grad_output_max = grad_output.abs().flatten().topk(100)[0][-1]
+        # grad_output, scale = _tensor_quant(grad_output, grad_output_max, 8, False, True)
+        # grad_output = (grad_output * scale).to(out_dtype)
+        grad_output = LSLinear.quant_transform(grad_output)
+        weight = LSLinear.quant_transform(weight)
+        grad_input = F.linear(grad_output, weight.T)
+        grad_input = LSLinear.quant_transform(grad_input)
+        inp = LSLinear.quant_transform(inp)
+
+        grad_weight = F.linear(
+            grad_output.reshape(-1, out_size).T, inp.reshape(-1, in_size).T
+        )
+        grad_weight = LSLinear.quant_transform(grad_weight)
+        # grad_input_max = grad_input.abs().amax(reduce_dim)
+        # # grad_input_max = grad_input.abs().float().quantile(0.9999)
+        # grad_input, scale = _tensor_quant(grad_input, grad_input_max, 8, False, True)
+        # grad_input = (grad_input * scale).to(out_dtype)
+
+        # grad_weight = LSLinear.quant_transform(grad_weight)
+        # grad_weight_max = grad_weight.abs().max()
+        # grad_weight_max = grad_weight.abs().amax(-1).unsqueeze(-1)
+        # grad_weight_max = grad_weight.abs().float().quantile(0.9999)
+        # grad_weight_max = grad_weight.abs().flatten().topk(100)[0][-1]
+        # grad_weight, scale = _tensor_quant(grad_weight, grad_weight_max, 8, False, True)
+        # grad_weight = (grad_weight * scale).to(out_dtype)
+        return (grad_input, grad_weight)
 
 
 act_quant_config = QuantDescriptor(
@@ -53,7 +110,14 @@ class QuantLinear(Linear):
         if self.input_quant is not None:
             qinput = self.input_quant(input)
         qweight = self.weight_quant(self.weight)
-        output = F.linear(qinput, qweight)
+
+        if self.weight_quant._disabled:
+            # print('fp16 linear forward')
+            output = F.linear(qinput, qweight)
+        else:
+            # print('quant linear forward')
+            output = LSLinear.apply(qinput, qweight)
+
         if self.output_quant is not None:
             output = self.output_quant(output)
         if self.bias is not None:
