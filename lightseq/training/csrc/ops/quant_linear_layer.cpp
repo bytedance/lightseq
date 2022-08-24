12d58ed7aa672e251d4a3cf9a02b9a65b95fb3b6
xiongying.taka <xiongying.taka@bytedance.com>
2022-08-24 10:38:35 +0800

    fix quant linear bug

--- lightseq/training/csrc/ops/quant_linear_layer.cpp
+++ lightseq/training/csrc/ops/quant_linear_layer.cpp
@@ -31,7 +31,7 @@ void QuantLinearLayer<T>::Forward(const T *inputs_ptr, const T *weight_ptr,
   int batch_tokens = _batch_size * _seq_len;
 
   if (_enable_quant) {
-    if (batch_tokens * _out_features % 8 == 0 || batch_tokens % 4 == 0 ||
+    if (_in_features % 8 == 0 && batch_tokens % 4 == 0 &&
         _out_features % 4 == 0) {
       int8_t *input_ptr_i8 = reinterpret_cast<int8_t *>(_quant_input_ptr);
       int8_t *weight_ptr_i8 = reinterpret_cast<int8_t *>(_quant_weight_ptr);
