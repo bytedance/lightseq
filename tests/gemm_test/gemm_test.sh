10fe4668418f3647ecf58ef645268b3186586650
weiyang.god <weiyang.god@bytedance.com>
2022-09-29 02:27:01 +0800

    add T4 igemm config

--- tests/gemm_test/gemm_test.sh
+++ tests/gemm_test/gemm_test.sh
@@ -1,4 +1,9 @@
 python3 gemm_test.py -hd 1024 -id 4096 -minb 1 -maxb 10000 -d configs
 python3 gemm_test.py -hd 512 -id 2048 -minb 1 -maxb 10000 -d configs
 python3 gemm_test.py -hd 768 -id 3072 -minb 1 -maxb 10000 -d configs
+
+if [ ! -d "/tmp/igemm_configs" ];then
+  mkdir /tmp/igemm_configs
+fi
+
 cp configs/* /tmp/igemm_configs
