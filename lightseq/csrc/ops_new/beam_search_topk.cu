a35a803a4be04b4e02f4eac9ce4b56d971890c5e
zhoubofan <zhoubofan@bytedance.com>
2022-11-09 19:39:22 +0800

    beam search operator

--- lightseq/csrc/ops_new/beam_search_topk.cu
+++ lightseq/csrc/ops_new/beam_search_topk.cu
@@ -4,30 +4,35 @@ namespace lightseq {
 
 template <typename T>
 
-BeamSearchTopOp<T>::BeamSearchTopOp(int max_batch_size, int max_step, int trg_vocab_size,
-                      int max_thread_per_block, int beam_size,
-                      int diverse_lambda, int end_id)
-: Operator("BeamSearchTopOp"),
-  _max_batch_size(max_batch_size),
-  _max_step(max_step),
-  _trg_vocab_size(trg_vocab_size),
-  _max_thread_per_block(max_thread_per_block),
-  _beam_size(beam_size),
-  _diverse_lambda(diverse_lambda),
-  _end_id(end_id),
-  _cub_sort_buffer_bytes(max_batch_size * beam_size * trg_vocab_size * sizeof(T)) {
-    
-    alive_seq.reset(new Variable("alive_seq"));
-    alive_seq->malloc_memory(max_batch_size * beam_size * sizeof(T));
-    alive_seq_buf.reset(new Variable("alive_seq_buf"));
-    alive_seq_buf->malloc_memory(max_batch_size * beam_size * sizeof(T));
-}
+BeamSearchTopOp<T>::BeamSearchTopOp(int nshared_dec_layer, int max_batch_size,
+                                    int max_step, int trg_vocab_size,
+                                    int hidden_size, int max_thread_per_block,
+                                    int beam_size, int diverse_lambda,
+                                    int dim_per_head, int end_id, int head_num)
+    : Operator("BeamSearchTopOp"),
+      _nshared_dec_layer(nshared_dec_layer),
+      _max_batch_size(max_batch_size),
+      _max_step(max_step),
+      _trg_vocab_size(trg_vocab_size),
+      _max_thread_per_block(max_thread_per_block),
+      _beam_size(beam_size),
+      _diverse_lambda(diverse_lambda),
+      _dim_per_head(dim_per_head),
+      _end_id(end_id),
+      _head_num(head_num),
+      _cub_sort_buffer_bytes(max_batch_size * beam_size * trg_vocab_size *
+                             sizeof(T)),
+      _cache_size(max_batch_size * max_step * hidden_size * _beam_size) {}
 
 template <typename T>
-std::tuple<Variable*, Variable*, Variable*> BeamSearchTopOp<T>::operator()(
-    Variable* logits, Variable* logit_bias, Variable* seq_probs,
-    Variable* seq_score, Variable* alive_seq) {
-  set_parents({logits, logit_bias, seq_probs, seq_score, alive_seq});
+std::tuple<Variable*, Variable*, Variable*, Variable*>
+BeamSearchTopOp<T>::operator()(Variable* logits, Variable* logit_bias,
+                               Variable* seq_probs, Variable* seq_score,
+                               Variable* alive_seq, Variable* caches_k,
+                               Variable* caches_k_buf, Variable* caches_v,
+                               Variable* caches_v_buf) {
+  set_parents({logits, logit_bias, seq_probs, seq_score, alive_seq, caches_k,
+               caches_k_buf, caches_v, caches_v_buf});
 
   Variable* can_idx = new Variable(
       "can_idx", _max_batch_size * _beam_size * _trg_vocab_size * sizeof(int));
@@ -37,22 +42,34 @@ std::tuple<Variable*, Variable*, Variable*> BeamSearchTopOp<T>::operator()(
   Variable* num_beam_can = new Variable(
       "num_beam_can", (_max_batch_size * _beam_size + 1) * sizeof(int));
 
-  set_children({can_idx, can_score, num_beam_can});
-  return std::make_tuple(can_idx, can_score, num_beam_can);
+  Variable* alive_seq_out = new Variable(
+      "alive_seq_out", _max_batch_size * _beam_size * _max_step * sizeof(int));
+
+  set_children({can_idx, can_score, num_beam_can, alive_seq_out});
+  return std::make_tuple(can_idx, can_score, num_beam_can, alive_seq_out);
 }
 
 template <typename T>
 void BeamSearchTopOp<T>::forward() {
   cudaStream_t stream = _context_ptr->get_stream();
-  T* logits_ptr = (T*)input(0)->value();
-  T* logits_bias_ptr = (T*)input(1)->value();
-  float* seq_probs_ptr = (float*)input(2)->value();
-  float* seq_score_ptr = (float*)input(3)->value();
-  int* alive_seq_ptr = (int*)input(4)->value();
+  T* logits_ptr = (T*)parent(0)->value();
+  T* logits_bias_ptr = (T*)parent(1)->value();
+  float* seq_probs_ptr = (float*)parent(2)->value();
+  float* seq_score_ptr = (float*)parent(3)->value();
+  int* alive_seq_ptr = (int*)parent(4)->value();
+  Variable* caches_k = parent(5);
+  Variable* caches_k_buf = parent(6);
+  Variable* caches_v = parent(7);
+  Variable* caches_v_buf = parent(8);
 
-  int* can_idx_ptr = (int*)output(0)->value();
-  float* can_score_ptr = (float*)output(1)->value();
-  int* num_beam_can_ptr = (int*)output(2)->value();
+  int* can_idx_ptr = (int*)child(0)->value();
+  float* can_score_ptr = (float*)child(1)->value();
+  int* num_beam_can_ptr = (int*)child(2)->value();
+  int* alive_seq_out = (int*)child(3)->value();
+
+  if (!_context_ptr->is_built()) {
+    return;
+  }
 
   /*
     step 1. logits bias and softmax,
@@ -97,6 +114,39 @@ void BeamSearchTopOp<T>::forward() {
   thrust::sort_by_key(thrust::cuda::par.on(stream), can_score_ptr,
                       can_score_ptr + _host_can_num_batch, can_idx_ptr,
                       thrust::greater<float>());
+
+  /*
+    step 3. refresh alive_seq, seq_probs, seq_score, num_finish_beam
+      based on sorted candidate.
+      Deciding whether early stop based on num_finish_beam
+  */
+  CHECK_GPU_ERROR(cudaMemsetAsync(num_beam_can_ptr, 0, sizeof(int), stream));
+  ker_refresh_result<<<dim3(_batch_size, _beam_size), _max_step, 0, stream>>>(
+      can_idx_ptr, can_score_ptr, num_beam_can_ptr + 1, alive_seq_ptr,
+      alive_seq_out, seq_probs_ptr, seq_score_ptr, num_beam_can_ptr,
+      _trg_vocab_size, _cur_step, _length_norm, _diverse_lambda, _end_id);
+  // swap alive_seq
+  Variable::swap_tensor(parent(4), child(3));
+  CHECK_GPU_ERROR(cudaMemcpyAsync(&_host_can_num_batch, num_beam_can_ptr,
+                                  sizeof(int), cudaMemcpyDefault, stream));
+  CHECK_GPU_ERROR(cudaStreamSynchronize(stream));
+
+  if (_host_can_num_batch == _step_token_num) {
+    return ;
+  }
+
+  /* ---step 4. refresh cache: k, v for decoder self attention--- */
+  if (_cur_step > 0) {
+    ker_refresh_cache_launcher<T>(
+        _nshared_dec_layer * (_cur_step + 1), _step_token_num * 2,
+        _max_thread_per_block, stream, num_beam_can_ptr + 1, can_idx_ptr,
+        (T*)caches_k->value(), (T*)caches_v->value(), (T*)caches_k_buf->value(),
+        (T*)caches_v_buf->value(), _cache_size, _beam_size, _dim_per_head,
+        _head_num, _trg_vocab_size, _cur_step, _max_step,
+        _diverse_lambda != 0, _end_id);
+    Variable::swap_tensor(caches_k, caches_k_buf);
+    Variable::swap_tensor(caches_v, caches_v_buf);
+  }
 }
 
 template class BeamSearchTopOp<float>;
--- lightseq/csrc/ops_new/includes/beam_search_topk.h
+++ lightseq/csrc/ops_new/includes/beam_search_topk.h
@@ -8,6 +8,7 @@ namespace lightseq {
 template <typename T>
 class BeamSearchTopOp : public Operator {
  private:
+ // inital
   int _max_batch_size;
   int _max_step;
   int _trg_vocab_size;
@@ -17,31 +18,43 @@ class BeamSearchTopOp : public Operator {
   int _max_thread_per_block;
   int _beam_size;
   float _diverse_lambda;
-  int _end_id;
+  int _nshared_dec_layer;
 
   size_t _cub_sort_buffer_bytes;
   int _host_can_num_batch;
+  int _batch_size;
+  int _cache_size;
+  int _end_id;
+  int _dim_per_head;
+  int _head_num;
 
   Variable* alive_seq;
   Variable* alive_seq_buf;
 
  public:
-  BeamSearchTopOp(int max_batch_size, int max_step, int trg_vocab_size,
-                      int max_thread_per_block, int beam_size,
-                      int diverse_lambda, int end_id);
+  BeamSearchTopOp(int nshared_dec_layer, int max_batch_size,
+                                    int max_step, int trg_vocab_size,
+                                    int hidden_size, int max_thread_per_block,
+                                    int beam_size, int diverse_lambda,
+                                    int dim_per_head, int end_id, int head_num);
 
   ~BeamSearchTopOp() {}
 
-  std::tuple<Variable*, Variable*, Variable*> operator()(Variable* logits,
-                                                         Variable* logit_bias,
-                                                         Variable* seq_probs,
-                                                         Variable* seq_score,
-                                                         Variable* alive_seq);
+  // output:
+  std::tuple<Variable*, Variable*, Variable*, Variable*> operator()(
+      Variable* logits, Variable* logit_bias,
+                               Variable* seq_probs, Variable* seq_score,
+                               Variable* alive_seq, Variable* caches_k,
+                               Variable* caches_k_buf, Variable* caches_v,
+                               Variable* caches_v_buf);
 
   void forward() override;
 
-  void before_forward(int length_norm, int cur_step, int step_token_num) {
-    _length_norm = length_norm, _cur_step = cur_step,
+  void before_forward(int batch_size, int length_norm, int cur_step,
+                      int step_token_num) {
+    _batch_size = batch_size;
+    _length_norm = length_norm;
+    _cur_step = cur_step;
     _step_token_num = step_token_num;
   }
 
