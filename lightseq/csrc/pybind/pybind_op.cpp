aa4ce4dc382cac18f9e5d34e2148759069f374d2
wangxiaohui.neo <wangxiaohui.neo@bytedance.com>
2023-02-14 11:54:26 +0800

    add split head for beam search

--- lightseq/csrc/pybind/pybind_op.cpp
+++ lightseq/csrc/pybind/pybind_op.cpp
@@ -52,14 +52,61 @@ void torch_split_head_op(const torch::Tensor& input, const torch::Tensor& bias,
   CHECK_GPU_ERROR(cudaGetLastError());
   print_time_duration(start, "op cost", 0);
 }
+/*
+  SplitHeadWithBeamOp(int max_batch_tokens, int num_heads, int hidden_size,
+                      int beam_size, int cache_len)
+  Variable* operator()(Variable* inp, Variable* bias, Variable* cache_k,
+                       Variable* cache_v);
+
+  void before_forward(int batch_size, int q_len, int step)
+*/
+template <typename T1, typename T2>
+void torch_split_head_with_beam_op(const torch::Tensor& input,
+                                   const torch::Tensor& bias,
+                                   torch::Tensor& query, torch::Tensor& key,
+                                   torch::Tensor& value, int batch_size,
+                                   int hidden_dim, int num_heads, int beam_size,
+                                   int q_len, int cache_len, int step) {
+  Context::create_global_context(StatusType::Inference);
+  std::shared_ptr<Context> context_ptr = Context::global_instance();
+
+  char* input_ptr = (char*)input.data_ptr();
+  char* bias_ptr = (char*)bias.data_ptr();
+  char* query_ptr = (char*)query.data_ptr();
+  char* key_ptr = (char*)key.data_ptr();
+  char* value_ptr = (char*)value.data_ptr();
+
+  Variable* input_var = new Variable("input", input_ptr);
+  Variable* bias_var = new Variable("bias", bias_ptr);
+  Variable* key_var = new Variable("key", key_ptr);
+  Variable* value_var = new Variable("value", value_ptr);
+
+  SplitHeadWithBeamOp<T1, T2>* op =
+      new SplitHeadWithBeamOp<T1, T2>(beam_size * batch_size * q_len, num_heads,
+                                      hidden_dim, beam_size, cache_len);
+
+  Variable* query_var = (*op)(input_var, bias_var, key_var, value_var);
+
+  query_var->set_value(query_ptr);
+
+  op->before_forward(batch_size, q_len, step);
+
+  context_ptr->build();
+  auto start = std::chrono::high_resolution_clock::now();
+  op->forward();
+  CHECK_GPU_ERROR(cudaGetLastError());
+  print_time_duration(start, "op cost", 0);
+}
 
 }  // namespace lightseq
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("torch_split_head_op_fp32",
-        &lightseq::torch_split_head_op<float, float>,
-        "note: torch_split_head_op_fp32");
+        &lightseq::torch_split_head_op<float, float>, "empty");
   m.def("torch_split_head_op_fp16",
-        &lightseq::torch_split_head_op<__half, __half>,
-        "note: torch_split_head_op_fp32");
+        &lightseq::torch_split_head_op<__half, __half>, "empty");
+  m.def("torch_split_head_with_beam_op_fp32",
+        &lightseq::torch_split_head_with_beam_op<float, float>, "empty");
+  m.def("torch_split_head_with_beam_op_fp16",
+        &lightseq::torch_split_head_with_beam_op<__half, __half>, "empty");
 }
