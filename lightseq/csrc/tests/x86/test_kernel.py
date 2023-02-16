1075e99ec05caeb2acfb2c333beb22f01c01528e
zhoubofan <zhoubofan@bytedance.com>
2023-02-16 13:58:07 +0800

    fix sys.path (#466)

--- lightseq/csrc/tests/x86/test_kernel.py
+++ lightseq/csrc/tests/x86/test_kernel.py
@@ -2,7 +2,7 @@ import os, sys
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 par_dir = os.path.dirname(cur_dir)
 sys.path.insert(0, par_dir)
-csrc_dir = os.path.dirname(cur_dir)
+csrc_dir = os.path.dirname(par_dir)
 sys.path.insert(0, csrc_dir)
 
 
