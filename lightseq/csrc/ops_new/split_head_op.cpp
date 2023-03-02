cc9b1f99019dbabf73a16f29fcbe9f1287872980
zhoubofan <zhoubofan@bytedance.com>
2023-03-02 19:11:19 +0800

    fix gpt attention layer compile problem

--- lightseq/csrc/ops_new/split_head_op.cpp
+++ lightseq/csrc/ops_new/split_head_op.cpp
@@ -7,13 +7,16 @@ std::tuple<Variable*, Variable*, Variable*> SplitHeadOp<T1, T2>::operator()(
     Variable* inp, Variable* bias) {
   set_parents({inp, bias});
   size_t trans_size = _max_batch_tokens * _hidden_size;
-  Variable* query = new Variable("splited_query", trans_size * sizeof(T1));
+  Variable* query =
+      new Variable("splited_query", trans_size, g_dtype<T1>(), g_dtype<T2>());
   if (_qkv_num == 1) {
     this->set_children({query});
     return std::make_tuple(query, nullptr, nullptr);
   }
-  Variable* key = new Variable("splited_key", trans_size * sizeof(T1));
-  Variable* value = new Variable("splited_value", trans_size * sizeof(T1));
+  Variable* key =
+      new Variable("splited_key", trans_size, g_dtype<T1>(), g_dtype<T2>());
+  Variable* value =
+      new Variable("splited_value", trans_size, g_dtype<T1>(), g_dtype<T2>());
   this->set_children({query, key, value});
   return std::make_tuple(query, key, value);
 }
@@ -37,8 +40,11 @@ void SplitHeadOp<T1, T2>::forward() {
     return;
   }
 
-  launch_split_head<T1>(inp_ptr, bias_ptr, q_ptr, k_ptr, v_ptr, _batch_size,
-                        _hidden_size, _head_dim, _seq_len, _qkv_num, _stream);
+#ifdef LIGHTSEQ_cuda
+  cuda::launch_split_head<T1>(inp_ptr, bias_ptr, q_ptr, k_ptr, v_ptr,
+                              _batch_size, _hidden_size, _head_dim, _seq_len,
+                              _qkv_num, _stream);
+#endif
 }
 
 template <typename T1, typename T2>
@@ -47,7 +53,9 @@ void SplitHeadOp<T1, T2>::backward() {
 }
 
 template class SplitHeadOp<float, float>;
+#ifdef LIGHTSEQ_cuda
 template class SplitHeadOp<__half, __half>;
+#endif
 
 template <typename T1, typename T2>
 Variable* SplitHeadWithBeamOp<T1, T2>::operator()(Variable* inp, Variable* bias,
@@ -55,7 +63,8 @@ Variable* SplitHeadWithBeamOp<T1, T2>::operator()(Variable* inp, Variable* bias,
                                                   Variable* cache_v) {
   set_parents({inp, bias, cache_k, cache_v});
   size_t trans_size = _max_batch_tokens * _hidden_size;
-  Variable* query = new Variable("splited_query", trans_size * sizeof(T1));
+  Variable* query =
+      new Variable("splited_query", trans_size, g_dtype<T1>(), g_dtype<T2>());
   this->set_children({query});
   return query;
 }
@@ -75,11 +84,15 @@ void SplitHeadWithBeamOp<T1, T2>::forward() {
     return;
   }
 
-  launch_split_head_with_beam<T1>(
+#ifdef LIGHTSEQ_cuda
+  cuda::launch_split_head_with_beam<T1>(
       inp_ptr, bias_ptr, q_ptr, k_ptr, v_ptr, _batch_size, _hidden_size,
       _head_dim, _beam_size, _q_len, _step, _cache_len, _stream);
+#endif
 }
 
 template class SplitHeadWithBeamOp<float, float>;
+#ifdef LIGHTSEQ_cuda
 template class SplitHeadWithBeamOp<__half, __half>;
+#endif
 }  // namespace lightseq
