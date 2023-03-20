270d6b09a3d1ab36419ee1ae739601f365a15f50
zhoubofan <zhoubofan@bytedance.com>
2023-03-20 11:31:12 +0800

    adapt scm compile

--- build.sh
+++ build.sh
@@ -1,7 +1,22 @@
-if [ ! -d 'build' ]; then
+#!/bin/bash
+
+export LIBRARY_PATH=/usr/local/cuda/lib64/stubs:${LIBRARY_PATH}
+export PATH=/opt/common_tools:/usr/local/cuda/bin:/usr/local/hdf5:${PATH}
+
+PROJECT_DIR=$(dirname $(readlink -e $0))
+cd $PROJECT_DIR
+
+if [[ ! -d "`pwd`/build" ]]; then
     mkdir build
 fi
-# DEVICE_ARCH could be cuda/x86/arm
-cd build && cmake -DUSE_NEW_ARCH=ON -DDEVICE_ARCH=cuda -DUSE_TRITONBACKEND=OFF -DDEBUG_MODE=OFF -DFP16_MODE=OFF -DMEM_DEBUG=OFF .. && make -j${nproc}
-# you can use comand like below to compile lightseq with pybind interface:
-# sudo PATH=$PATH:/usr/local/hdf5 CUDACXX=/usr/local/cuda/bin/nvcc  DEVICE_ARCH=cuda ENABLE_FP32=1 ENABLE_DEBUG=0 ENABLE_NEW_ARCH=1 python3 setup.py install
+
+cd build && \
+cmake -DCMAKE_BUILD_TYPE=Release -DFP16_MODE=ON -DUSE_NEW_ARCH=ON -DDEBUG_MODE=OFF -DDYNAMIC_API=ON .. && \
+make -j${nproc} && \
+mkdir $PROJECT_DIR/output && \
+mkdir $PROJECT_DIR/output/lib && \
+cp $PROJECT_DIR/build/lightseq/csrc/models/liblightseq.so $PROJECT_DIR/output && \
+cp -av /usr/local/cuda/lib64/libcublas.so* $PROJECT_DIR/output/lib && \
+cp -av /usr/local/cuda/lib64/libcublasLt.so* $PROJECT_DIR/output/lib && \
+cp -av /usr/local/cuda/lib64/libcudart.so* $PROJECT_DIR/output/lib && \
+cp $PROJECT_DIR/run.sh $PROJECT_DIR/output
