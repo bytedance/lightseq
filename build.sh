931f6a83c4ffd444d07de5d038e09220e45a6a73
zhoubofan <zhoubofan@bytedance.com>
2023-03-20 15:36:34 +0800

    fix compile error

--- build.sh
+++ build.sh
@@ -1,22 +1,7 @@
-#!/bin/bash
-
-export LIBRARY_PATH=/usr/local/cuda/lib64/stubs:${LIBRARY_PATH}
-export PATH=/opt/common_tools:/usr/local/cuda/bin:/usr/local/hdf5:${PATH}
-export LD_LIBRARY_PATH=/usr/local/cuda/compat:/usr/local/nvidia/lib:/usr/local/nvidia/lib64:/usr/local/cuda/lib64:${LD_LIBRARY_PATH}
-
-PROJECT_DIR=$(dirname $(readlink -e $0))
-cd $PROJECT_DIR
-
-if [[ ! -d "`pwd`/build" ]]; then
+if [ ! -d 'build' ]; then
     mkdir build
 fi
-
-cd build && \
-cmake -DCMAKE_BUILD_TYPE=Release -DUSE_NEW_ARCH=ON -DDEVICE_ARCH=cuda -DUSE_TRITONBACKEND=ON -DDEBUG_MODE=OFF -DFP16_MODE=ON -DMEM_DEBUG=OFF -DDYNAMIC_API=ON .. && \
-make -j${nproc} && \
-mkdir $PROJECT_DIR/output && \
-mkdir $PROJECT_DIR/output/lib && \
-cp $PROJECT_DIR/build/lightseq/csrc/models/liblightseq.so $PROJECT_DIR/output && \
-cp -av /usr/local/cuda/lib64/libcublas.so* $PROJECT_DIR/output/lib && \
-cp -av /usr/local/cuda/lib64/libcublasLt.so* $PROJECT_DIR/output/lib && \
-cp -av /usr/local/cuda/lib64/libcudart.so* $PROJECT_DIR/output/lib
+# DEVICE_ARCH could be cuda/x86/arm
+cd build && cmake -DUSE_NEW_ARCH=ON -DDEVICE_ARCH=cuda -DUSE_TRITONBACKEND=OFF -DDEBUG_MODE=OFF -DFP16_MODE=OFF -DMEM_DEBUG=OFF .. && make -j${nproc}
+# you can use comand like below to compile lightseq with pybind interface:
+# sudo PATH=$PATH:/usr/local/hdf5 CUDACXX=/usr/local/cuda/bin/nvcc  DEVICE_ARCH=cuda ENABLE_FP32=1 ENABLE_DEBUG=0 ENABLE_NEW_ARCH=1 python3 setup.py install
